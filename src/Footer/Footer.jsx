97f8ea2729ba424a9d877cd8a8c283eaa7a108f2
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2023-08-22 23:22:51 +0300

    Updates

--- src/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer className="App-footer">
-      <p>&copy; 2023 Sushi. All rights reserved.</p>
-    </footer>
-  );
-}
-export default Footer;
--- src/MenuPage/Place.jsx
+++
@@ -1,9 +0,0 @@
-function Place() {
-  return (
-    <>
-      <div className="App App-header logoFontSmall">Place</div>
-    </>
-  );
-}
-
-export default Place;
